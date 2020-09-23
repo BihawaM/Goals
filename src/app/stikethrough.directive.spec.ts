c8b11ed6b0b6910061ac8ed844a4da861611fedb
BihawaM <bihawam271@gmail.com>
2020-09-23 17:03:28 +0300

    initial commit

---
+++ src/app/stikethrough.directive.spec.ts
@@ -0,0 +1,8 @@
+import { StikethroughDirective } from './stikethrough.directive';
+
+describe('StikethroughDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StikethroughDirective();
+    expect(directive).toBeTruthy();
+  });
+});
